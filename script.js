159d53a29c08559cde0b91df45135b11b4cf6f45
OnyxCipher <39405724+OnyxCipher@users.noreply.github.com>
2022-10-07 22:37:57 -0500

    Adding to script file

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
+let i = 0;
+let text = 'My goal is to work everyday to solve problems for businesses and organizations. Clients and customers are at the heart of any success.';
+    const textSpeed = 45;
 
+function typingKeys() {
+  if (i < text.length) {
+    document.getElementById("typedInput").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typingKeys, textSpeed);
+  }
+}
 
-const api_url ="https://zenquotes.io/api/quotes/";
-
- async function getmyapi() {
- 
- const response = await fetch();
-   var data = await response.json();
-  console.log(data);}
-
-
- getmyapi(api_url);
-
-
-
-
-
- fetch()
- .then(res => {
-   return res.json();
- })
+// I really liked the Javascript effect of typing text on the page itself..I found a cool reference at https://codepen.io/iion91/pen/NWqBdgq //
